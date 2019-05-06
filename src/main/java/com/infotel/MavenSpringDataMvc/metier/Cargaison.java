0f4fb58416097eeed920d0be41c23a4668501d6f
IN-MP-026 <joanainespirescunha@gmail.com>
2019-05-06 11:37:01 +0200

    cargaison

--- src/main/java/com/infotel/MavenSpringDataMvc/metier/Cargaison.java
+++ src/main/java/com/infotel/MavenSpringDataMvc/metier/Cargaison.java
@@ -1,14 +1,14 @@
 package com.infotel.MavenSpringDataMvc.metier;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.springframework.stereotype.Component;
 
-@Table (name="Cargaison")
+@Table (name="cargaison")
 @Component
 public abstract class Cargaison {
 	
@@ -18,6 +18,55 @@ public abstract class Cargaison {
 	protected String nomCargaison;
 	protected String depart;
 	protected String destination;
+	
+	@ManyToOne
+	private SocieteTransport societeTransport;
+
+	public int getIdCargaison() {
+		return idCargaison;
+	}
+
+	public void setIdCargaison(int idCargaison) {
+		this.idCargaison = idCargaison;
+	}
+
+	public String getNomCargaison() {
+		return nomCargaison;
+	}
+
+	public void setNomCargaison(String nomCargaison) {
+		this.nomCargaison = nomCargaison;
+	}
+
+	public String getDepart() {
+		return depart;
+	}
+
+	public void setDepart(String depart) {
+		this.depart = depart;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public SocieteTransport getSocieteTransport() {
+		return societeTransport;
+	}
+
+	public void setSocieteTransport(SocieteTransport societeTransport) {
+		this.societeTransport = societeTransport;
+	}
+
+	@Override
+	public String toString() {
+		return "Cargaison [idCargaison=" + idCargaison + ", nomCargaison=" + nomCargaison + ", depart=" + depart
+				+ ", destination=" + destination + "]";
+	}
 	
 	
 }
